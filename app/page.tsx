cebaeca8d23e931f5da86dc90922a9746d197c6f
artalfred <bernalesartalfred.com>
2024-11-18 11:28:55 +0800

    Fix Errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
 import Link from "next/link";
-import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import Blogs from "@/public/Images/Blogs.jpg";
+import { authOptions } from "./api/auth/authOptions";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -12,7 +9,6 @@ export default async function Home() {
     <main>
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">UsersPage</Link>
-      <ProductCard />
     </main>
   );
 }
